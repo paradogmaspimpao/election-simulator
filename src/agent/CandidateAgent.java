951bcac0eff7092df5a1140680bfc0f7b4d44fcc
Mateus Nóbrega <mateusn@outlook.com>
2018-06-17 18:46:13 -0300

    Added candidate agent

---
+++ src/agent/CandidateAgent.java
@@ -0,0 +1,47 @@
+package agent;
+
+import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+
+public class CandidateAgent extends Agent
+{
+	private static final long serialVersionUID = 7951150000544730740L;
+	
+	@Override
+	protected void setup() 
+	{
+		System.out.println("My name is " + getAID().getName());
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("Candidate ");
+		sd.setName(getLocalName() + "-candidate");
+		
+		dfd.addServices(sd);
+		
+		try {
+			DFService.register(this, dfd);
+			System.out.println("I'm registered in the DF");
+		}
+		catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+
+	}
+	
+	@Override
+	protected void takeDown() 
+	{
+		try {
+			DFService.deregister(this);
+			System.out.println("I'm removed from the DF");
+		}catch(FIPAException fe) {
+			fe.printStackTrace();
+		}
+	}
+
+}
