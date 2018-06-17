b8ae16750dad5f04121579b8ce19ed8ab27b1010
Mateus Nóbrega <mateusn@outlook.com>
2018-06-17 12:03:20 -0300

    Added user interaction

--- src/agent/Voter.java
+++ src/agent/Voter.java
@@ -1,4 +1,8 @@
 package agent;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 import jade.core.*; 
 import jade.core.behaviours.*; 
 import jade.lang.acl.ACLMessage;
@@ -8,18 +12,38 @@ public class Voter extends Agent{
 private static final long serialVersionUID = -6461105609727551278L;
 
 	class Vote extends OneShotBehaviour {
+		
+		public Vote(Agent a)
+		{
+			super(a);
+		}
+		
+		private String printDate()
+		{
+			long epoch = System.currentTimeMillis();
+			SimpleDateFormat date = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+			Date r_date = new Date(epoch);
+			
+			return date.format(r_date);
+		}
 	
 		private static final long serialVersionUID = -1447880119921839792L;
 		
 		private String targetCandidate;
+		private Scanner sc = new Scanner(System.in);
 		
 		public void action() {
-			System.out.println("Votando");
+			System.out.println("Insira o seu candidato: ");
+			targetCandidate = sc.nextLine();
+			
 			ACLMessage vote = new ACLMessage(ACLMessage.INFORM);
-			vote.addReceiver(new AID("VotingMachine", AID.ISLOCALNAME));
+			
+			vote.addReceiver(new AID("urna", AID.ISLOCALNAME));
 			vote.setContent(targetCandidate);
 			vote.setConversationId("voto");
-			vote.setReplyWith("Voto em " + targetCandidate + System.currentTimeMillis());
+			vote.setReplyWith("Voto em " + targetCandidate + " " + printDate());
+			
+			System.out.println("Sending vote...");
 			myAgent.send(vote);
 		}
 	}
@@ -27,6 +51,7 @@ private static final long serialVersionUID = -6461105609727551278L;
 	@Override
 	protected void setup() {
 		System.out.println("Initializing Voting Machine.");
+		addBehaviour(new Vote(this));
 	}
   
 	@Override
--- src/agent/VotingMachine.java
+++ src/agent/VotingMachine.java
@@ -1,20 +1,62 @@
 package agent;
 import jade.core.*; 
-import jade.core.behaviours.*; 
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage; 
 
-public class VotingMachine extends Agent{
-	
-	
+public class VotingMachine extends Agent
+{	
 	private static final long serialVersionUID = -3657633911205663525L;
 
-	
-	  @Override
-	  protected void setup() {
-	    System.out.println("Initializing Voting Machine.");
-	  }
-	  
-	  @Override
-	  protected void takeDown() {
-	    System.out.println("Closing Voting Machine.");
-	  }
+	private class receiveVotes extends Behaviour
+	{
+		private int numberOfMessages = 0;
+		
+		public receiveVotes(Agent a)
+		{
+			super(a);
+		}
+		
+		@Override
+		public boolean done() {
+			if (numberOfMessages == 3) 
+			{
+				return true;
+			}
+			
+			return false;
+		}
+		@Override
+		public void action() {
+			ACLMessage message = myAgent.receive();
+			
+			if (message != null)
+			{
+				System.out.println("I received a message!");
+				String content = message.getContent();
+				System.out.println("CONTENT: " +  content);
+				System.out.println("REPLY: " + message.getReplyWith());
+				numberOfMessages++;
+			}
+			else
+			{
+				block();
+			}
+			
+		}
+		
+	}
+
+  @Override
+  protected void setup() 
+  {
+	  System.out.println("Initializing the system");
+	  receiveVotes rv = new receiveVotes(this);
+	  addBehaviour(rv);
+  }
+  
+  @Override
+  protected void takeDown() 
+  {
+    System.out.println("Closing Voting Machine...");
+  }
 }
