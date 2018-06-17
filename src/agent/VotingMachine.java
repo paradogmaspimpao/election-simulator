0825360c661f2ee84df417d27472da97111b3bc7
Samuel Borges <samuelmordred@gmail.com>
2018-06-17 16:42:15 -0300

    added candidate hashtable and barebones voting logic

--- src/agent/VotingMachine.java
+++ src/agent/VotingMachine.java
@@ -1,12 +1,27 @@
 package agent;
 import jade.core.*; 
 import jade.core.behaviours.*;
-import jade.lang.acl.ACLMessage; 
+import jade.lang.acl.ACLMessage;
+import java.util.*;
 
 public class VotingMachine extends Agent
 {	
+	
+	private Hashtable candidates;
+	
+	private class Candidates {
+		
+		Candidates(){
+			candidates = new Hashtable();
+			candidates.put("Bolsonaro", 0);
+			candidates.put("Ciro", 0);
+			candidates.put("Meirelles", 0);
+		}
+	}
+	
 	private static final long serialVersionUID = -3657633911205663525L;
-
+	
+	
 	private class receiveVotes extends Behaviour
 	{
 		private int numberOfMessages = 0;
@@ -18,8 +33,20 @@ public class VotingMachine extends Agent
 		
 		@Override
 		public boolean done() {
+			int max = 0;
+			String vencedor = null;
 			if (numberOfMessages == 3) 
 			{
+				Enumeration<String> enumeration = candidates.keys();
+				while(enumeration.hasMoreElements()) {
+		            String key = enumeration.nextElement();
+		            if((int)candidates.get(key) > max){
+		            	vencedor = key;
+		            	max = (int)candidates.get(key);
+		            }
+		            System.out.println("Candidato: " + key + "\tQuantidade de votos: " + candidates.get(key));
+		        }
+				System.out.println("Vencedor: " + vencedor);
 				return true;
 			}
 			
@@ -33,6 +60,10 @@ public class VotingMachine extends Agent
 			{
 				System.out.println("I received a message!");
 				String content = message.getContent();
+
+				//incrementa votos
+				candidates.put(content, (int)candidates.get(content)+1);
+
 				System.out.println("CONTENT: " +  content);
 				System.out.println("REPLY: " + message.getReplyWith());
 				numberOfMessages++;
@@ -45,11 +76,13 @@ public class VotingMachine extends Agent
 		}
 		
 	}
-
+	
+	
   @Override
   protected void setup() 
   {
 	  System.out.println("Initializing the system");
+	  new Candidates();
 	  receiveVotes rv = new receiveVotes(this);
 	  addBehaviour(rv);
   }
